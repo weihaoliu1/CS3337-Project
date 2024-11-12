6b3c8d0e3f5b68eae754dabfcfb618a94c73f44b
Michael Ramos <124205129+mramosr2@users.noreply.github.com>
2024-11-12 14:16:44 -0800

    Add files via upload

---
+++ CS3337S1/bookEx/bookMng/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home', views.index, name='index'),
+    path('postbook', views.postbook, name='postbook'),
+    path('displaybooks', views.displaybooks, name='displaybooks'),
+    path('mybooks', views.mybooks, name='mybooks'),
+    path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
+    path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
+    path('search', views.search_books, name='search_books'),
+]
+
+
