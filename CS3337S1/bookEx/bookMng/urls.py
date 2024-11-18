d3e434cbe081fac8fc9367ec5730bf026e719678
Michael Ramos <124205129+mramosr2@users.noreply.github.com>
2024-11-18 00:21:59 -0800

    Update urls.py

--- CS3337S1/bookEx/bookMng/urls.py
+++ CS3337S1/bookEx/bookMng/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
     path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
     path('search', views.search_books, name='search_books'),
+    path('rate_book/<int:book_id>/', views.rate_book, name='rate_book'),
 ]
 
 
