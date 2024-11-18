ad805c7841920437c59be55aee8cdfaf82a42992
Michael Ramos <124205129+mramosr2@users.noreply.github.com>
2024-11-18 00:23:26 -0800

    Update urls.py

--- CS3337S1/bookEx/bookMng/urls.py
+++ CS3337S1/bookEx/bookMng/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
     path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
     path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
     path('search', views.search_books, name='search_books'),
-    path('rate_book/<int:book_id>/', views.rate_book, name='rate_book'),
+    path('rate_book/<int:book_id>/', views.rate_book, name='rate_book'), 
+    # *NOTE* When Implmenting Ratings run the following commands in PyCharm Terminal
+    # python manage.py makemigrations
+    # python manage.py migrate
 ]
 
 
