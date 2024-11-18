3da082eba03a11ef8e78952f7011a07109b268b8
Michael Ramos <124205129+mramosr2@users.noreply.github.com>
2024-11-18 02:37:40 -0800

    Update admin.py

--- CS3337S1/bookEx/bookMng/admin.py
+++ CS3337S1/bookEx/bookMng/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import MainMenu
+from .models import MainMenu, Comment
 
 from .models import Book
 
 admin.site.register(MainMenu)
 admin.site.register(Book)
-
+admin.site.register(Comment)
